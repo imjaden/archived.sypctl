19ac9f5f992c0262e7d5e1b1a1d970a6067851ac
jay16 <jay_li@intfocus.com>
2018-02-10 21:51:51 +0800

    feat(bash): 基本部署功能

--- tools.sh
+++ tools.sh
@@ -10,8 +10,9 @@ case "$1" in
         echo
         echo '## deploy tomcat'
         echo
-        bash lib/bash/tomcat-tools.sh /usr/local/src/tomcatAPI install 8080
-        bash lib/bash/tomcat-tools.sh /usr/local/src/tomcatAdmin install 8081
+        bash lib/bash/tomcat-tools.sh /usr/local/src/tomcatAPI install 8081
+        bash lib/bash/tomcat-tools.sh /usr/local/src/tomcatSuperAdmin install 8082
+        bash lib/bash/tomcat-tools.sh /usr/local/src/tomcatAdmin install 8083
 
         echo
         echo '## deploy zookeeper'
@@ -21,6 +22,7 @@ case "$1" in
     start|stop|status|restart|monitor)
         bash lib/bash/tomcat-tools.sh /usr/local/src/tomcatAPI $1
         bash lib/bash/tomcat-tools.sh /usr/local/src/tomcatAdmin $1
+        bash lib/bash/tomcat-tools.sh /usr/local/src/tomcatSuperAdmin $1
         bash lib/bash/zookeeper-tools.sh /usr/local/src/zookeeper $1
     ;;
     *)
