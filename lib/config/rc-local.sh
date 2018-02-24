c9f44ffb2a1dfac560b761b01057ad3f67988cf8
jay16 <jay_li@intfocus.com>
2018-02-24 22:43:08 +0800

    fix(rc.local): 完善开机启动脚本

--- lib/config/rc-local.sh
+++ lib/config/rc-local.sh
@@ -7,4 +7,8 @@
 cd /opt/scripts/syp-saas-scripts
 sudo -u root bash tools.sh monitor 
 sudo -u root crontab lib/config/syp@crontab.conf
+
+cd /root/www/shengyiplus-server 
+sudo -u root bash tool.sh start
+sudo -u root bash tool.sh crontab:update
 # <<<<<<<<<<<<<<<<<<<< 脚本结束 <<<<<<<<<<<<<<<<<<<<
